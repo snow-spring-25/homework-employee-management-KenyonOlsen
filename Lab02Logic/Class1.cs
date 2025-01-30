8fdfa13fb49cba823bdf384aad23a193de48fa1d
Kenyon Olsen <kenyon.olsen@students.snow.edu>
2025-01-30 10:20:31 -0700

    Leaving to class

---
+++ Lab02Logic/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Dynamic;
+using System.Reflection;
+using Microsoft.VisualBasic;
+
+namespace Lab02Logic;
+
+public class Employee
+{
+    public Employee()
+    {
+
+    }
+    public Employee(string firstName, string lastName)
+    {
+        FirstName=firstName;
+        LastName=lastName;
+    }
+
+    string? FirstName{get; set;}
+    string? LastName{get; set;}
+    decimal HourlyPay{get; set;}
+    decimal HoursWorked{get; set;}
+    decimal TotalPay{get;}
+
+
+
+
+}
